eab4ec7dbd8ceb6d2dbc0e11ab2241984dfaede5
chweeks <chrisharryweeks@gmail.com>
2015-12-14 13:34:49 +0000

    delete completed tasks button and methods added

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -16,7 +16,21 @@ Todos.TodosController = Ember.ArrayController.extend({
 
       // Save the new model
       todo.save();
-    }
+    },
+
+    clearCompleted: function() {
+      var completed = this.filterBy("isCompleted", true);
+      completed.invoke("deleteRecord");
+      completed.invoke("save");
+    },
+
+    hasCompleted: function() {
+      return this.get("completed") > 0;
+    }.property("completed"),
+
+    completed: function(){
+      return this.filterBy("isCompleted", true).get("length");
+    }.property("@each.isCompleted")
   },
 
   remaining: function() {
@@ -26,7 +40,7 @@ Todos.TodosController = Ember.ArrayController.extend({
   inflection: function() {
     var remaining = this.get("remaining");
     return remaining === 1 ? "item" : "items";
-  }.property("remaining")
+  }.property("remaining"),
 
 
 });
