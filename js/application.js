f15d638e0712b1ae2ca0d9ce2821d0e636675e2e
chweeks <chrisharryweeks@gmail.com>
2015-12-29 18:22:44 +0000

    loaded other pokemon data

--- js/application.js
+++ js/application.js
@@ -5,9 +5,84 @@ Pokedex.ApplicationAdapter = DS.FixtureAdapter.extend();
 Pokedex.Item = Ember.Object.extend();
 
 Pokedex.Item.reopenClass({
-  all: function() {
+  bulbasaur: function() {
     return $.getJSON("http://pokeapi.co/api/v1/pokemon/1/").then(function(response) {
       return response
     });
-  }
+  },
+  ivysaur: function() {
+    return $.getJSON("http://pokeapi.co/api/v1/pokemon/2/").then(function(response) {
+      return response
+    });
+  },
+  venusaur: function() {
+    return $.getJSON("http://pokeapi.co/api/v1/pokemon/3/").then(function(response) {
+      return response
+    });
+  },
+  charmander: function() {
+    return $.getJSON("http://pokeapi.co/api/v1/pokemon/4/").then(function(response) {
+      return response
+    });
+  },
+  charmeleon: function() {
+    return $.getJSON("http://pokeapi.co/api/v1/pokemon/5/").then(function(response) {
+      return response
+    });
+  },
+  charizard: function() {
+    return $.getJSON("http://pokeapi.co/api/v1/pokemon/6/").then(function(response) {
+      return response
+    });
+  },
+  squirtle: function() {
+    return $.getJSON("http://pokeapi.co/api/v1/pokemon/7/").then(function(response) {
+      return response
+    });
+  },
+  wartortle: function() {
+    return $.getJSON("http://pokeapi.co/api/v1/pokemon/8/").then(function(response) {
+      return response
+    });
+  },
+  blastoise: function() {
+    return $.getJSON("http://pokeapi.co/api/v1/pokemon/9/").then(function(response) {
+      return response
+    });
+  },
+  caterpie: function() {
+    return $.getJSON("http://pokeapi.co/api/v1/pokemon/10/").then(function(response) {
+      return response
+    });
+  },
+  metapod: function() {
+    return $.getJSON("http://pokeapi.co/api/v1/pokemon/11/").then(function(response) {
+      return response
+    });
+  },
+  butterfree: function() {
+    return $.getJSON("http://pokeapi.co/api/v1/pokemon/12/").then(function(response) {
+      return response
+    });
+  },
+  weedle: function() {
+    return $.getJSON("http://pokeapi.co/api/v1/pokemon/13/").then(function(response) {
+      return response
+    });
+  },
+  kakuna: function() {
+    return $.getJSON("http://pokeapi.co/api/v1/pokemon/14/").then(function(response) {
+      return response
+    });
+  },
+  beedrill: function() {
+    return $.getJSON("http://pokeapi.co/api/v1/pokemon/15/").then(function(response) {
+      return response
+    });
+  },
+  pidgey: function() {
+    return $.getJSON("http://pokeapi.co/api/v1/pokemon/16/").then(function(response) {
+      return response
+    });
+  },
 });
--- js/router.js
+++ js/router.js
@@ -6,7 +6,22 @@ Pokedex.Router.map(function() {
 Pokedex.PokedexRoute = Ember.Route.extend({
   model: function() {
     return Ember.RSVP.hash({
-      bulbasaur: Pokedex.Item.all()
+      bulbasaur: Pokedex.Item.bulbasaur(),
+      ivysaur: Pokedex.Item.ivysaur(),
+      venusaur: Pokedex.Item.venusaur(),
+      charmander: Pokedex.Item.charmander(),
+      charmeleon: Pokedex.Item.charmeleon(),
+      charizard: Pokedex.Item.charizard(),
+      squirtle: Pokedex.Item.squirtle(),
+      wartortle: Pokedex.Item.wartortle(),
+      blastoise: Pokedex.Item.blastoise(),
+      caterpie: Pokedex.Item.caterpie(),
+      metapod: Pokedex.Item.metapod(),
+      butterfree: Pokedex.Item.butterfree(),
+      weedle: Pokedex.Item.weedle(),
+      kakuna: Pokedex.Item.kakuna(),
+      beedrill: Pokedex.Item.beedrill(),
+      pidgey: Pokedex.Item.pidgey()
     });
   }
 });
